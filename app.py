ef3b9f5073f807c3ed00fcb7a2a62cfb6625aa5b
Prince Kurien <princevkurien@gmail.com>
2022-06-03 19:38:44 +0400

    test

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from app_pipeline.prediction_pipeline import PredictionPipeline
-from app_config.constants import *
-from app_util.util import read_yaml_file
+# from app_pipeline.prediction_pipeline import PredictionPipeline
+# from app_config.constants import *
+# from app_util.util import read_yaml_file
 from flask import Flask, request
 from flask import send_file, abort, render_template ,request, jsonify
 from wsgiref import simple_server
@@ -11,17 +11,17 @@ import os
 import pandas as pd 
 
 
-config_info = read_yaml_file(file_path=CONFIG_FILE_PATH )
-model_pusher_config = config_info[MODEL_PUSHER_CONFIG_KEY]
-data_validation_config = config_info[DATA_VALIDATION_CONFIG_KEY]
-validation_config_dir = data_validation_config[DATA_VALIDATION_CONFIG_DIR]
-model_dir = os.path.join(ROOT_DIR, model_pusher_config[MODEL_PUSHER_MODEL_EXPORT_DIR_KEY])
-schema_file_path = os.path.join(
-    ROOT_DIR, validation_config_dir, data_validation_config[DATA_VALIDATION_SCHEMA_FILE_NAME_KEY])
-columns_transformer_dir = os.path.join(ROOT_DIR , model_pusher_config[PREPROCESSING_EXPORT_DIR_KEY])
-prediction_pipeline_obj = PredictionPipeline(model_dir=model_dir,\
-                                        columns_transformer_dir=columns_transformer_dir,\
-                                        schema_file_path=schema_file_path)
+# config_info = read_yaml_file(file_path=CONFIG_FILE_PATH )
+# model_pusher_config = config_info[MODEL_PUSHER_CONFIG_KEY]
+# data_validation_config = config_info[DATA_VALIDATION_CONFIG_KEY]
+# validation_config_dir = data_validation_config[DATA_VALIDATION_CONFIG_DIR]
+# model_dir = os.path.join(ROOT_DIR, model_pusher_config[MODEL_PUSHER_MODEL_EXPORT_DIR_KEY])
+# schema_file_path = os.path.join(
+#     ROOT_DIR, validation_config_dir, data_validation_config[DATA_VALIDATION_SCHEMA_FILE_NAME_KEY])
+# columns_transformer_dir = os.path.join(ROOT_DIR , model_pusher_config[PREPROCESSING_EXPORT_DIR_KEY])
+# prediction_pipeline_obj = PredictionPipeline(model_dir=model_dir,\
+#                                         columns_transformer_dir=columns_transformer_dir,\
+#                                         schema_file_path=schema_file_path)
 
 app = Flask(__name__) # create the Flask app
 
@@ -37,7 +37,7 @@ def index():
     except Exception as e:
         return jsonify({'status': str(e)})
 
-@app.route('/eda', methods=['POST'])
+@app.route('/eda', methods=['POST', 'GET'])
 @cross_origin()
 def eda():
     try:
@@ -45,7 +45,7 @@ def eda():
     except Exception as e:
         return jsonify({'status': str(e)})
 
-@app.route('/uploader', methods=['POST'])
+@app.route('/uploader',methods=['POST', 'GET'])
 @cross_origin()
 def predictfile():
 
@@ -65,7 +65,7 @@ def predictfile():
     except Exception as e:
         return str(e)
         
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['POST', 'GET'])
 @cross_origin()
 def predict():
     try:
