2904e9bdf7b89dcf896b3cbbce8259c5af256832
Prince Kurien <princevkurien@gmail.com>
2022-06-03 20:22:39 +0400

    Revert "flask_config"
    This reverts commit 744f315be7ec98d82ffabd607bce3094312b21a5.

--- app.py
+++ app.py
@@ -1,84 +1,62 @@
-# doing necessary imports
-
-import threading
-import io
-import time
-from flask import Flask, render_template, request, jsonify, Response, url_for, redirect
+from app_config.configuration import AppConfiguration
+from app_exception.exception import AppException
+from app_entity.config_entity import FlaskConfig
+from flask import Flask, request
+from flask import send_file, abort, render_template ,request, jsonify
+from wsgiref import simple_server
+from flask import Flask, request, render_template
+from flask import Response
 from flask_cors import CORS, cross_origin
+import pandas as pd
+import os 
 
-
-master_db_name = "check1223"
-
-
-# To avoid the time out issue on heroku
-class threadClass:
-
-    def __init__(self ):
-        global master_db_name
-        thread = threading.Thread(target=self.run, args=())
-        thread.daemon = True  # Daemonize thread
-        thread.start()  # Start the execution
-    
-    def run(self):
-        global master_db_name
-        scrapper = Run(db_name=master_db_name)
-        logging.info("Thread run completed")
-        logging.debug(scrapper)
-
+AppConfiguration = AppConfiguration()
+prediction_config = AppConfiguration.get_flask_config()
 
 
 
 app = Flask(__name__)  # initialising the flask app with the name 'app'
+CORS(app)
 
 @app.route('/', methods=['POST', 'GET'])
 @cross_origin()
 def index():
     return render_template('index.html')
 
-@app.route('/test', methods=['POST', 'GET'])
-@cross_origin()
-def index1():
+@app.route('/uploader', methods=['POST'])
+def predictfile():
+    try:
+        if request.method == 'POST':
+            file = request.files['file']
+            if file is not None:
+               
+                df = pd.read_csv(file)
+                print(df)
 
-    mongodb_master_collection = MongoDB(db_name, 'Final')
-    master_data_course = set() 
-    master_data_response = []
-    
-    if request.method == 'POST':
-        Selected_attribute = request.form.get('attribute')
-        Selected_Category = request.form['category']
-        Selected_Course = request.form['course']
-        if Selected_Course is None:
-            logging.info(f'No Course Selected  Selected category {Selected_Category}')
-            master_data = mongodb_master_collection.find_many({'category' : Selected_Category})
-            for data in master_data:
-                master_data_course.add(data['course_name'])
-                logging.info(f'course Retrived for category {Selected_Category}')
-                
-            return render_template('index.html', course_attributes = Selected_attribute ,
-                               course_category = Selected_Category, course_coursename = master_data_course)
-        else:
-            master_data_response = mongodb_master_collection.find_one({'course_name' : Selected_Course})
-            logging.info(f'Course Selected {Selected_Course} selected category {Selected_Category} attribute {Selected_attribute}')
-            response_data = master_data_response["Selected_attribute"]
-            return Response(response=response_data, status=200)
-
-    else:
-        master_data = mongodb_master_collection.find_many()
-        master_data_attribute = ['description', 'price', 'course_features', 'what_youll_learn', 'timings',
-                    'requirements', 'course_curriculum_dict', 'mentor_names', 'category', 'course_link']
-        master_data_category = [data['category'] for data in master_data]
-        logging.info(f'Master Data Category {master_data_category}')
-        return render_template('index.html' , course_attributes = master_data_attribute ,
-                               course_category = master_data_category )
+                prediction = prediction_config.prediction_pipeline_obj.predict(df)
+                print(prediction)
+                return send_file(prediction, as_attachment=True)
+            else:
+                return Response(status=400)
+    except Exception as e:
+        return str(e)
+        
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        if request.method == 'POST':
+            data = request.form.to_dict()
+            df = pd.DataFrame(data , index = [0])
+            print(df)
+            prediction = prediction_config.prediction_pipeline_obj.predict(df)
+            
+            return send_file(prediction, as_attachment=True)
+            
+    except Exception as e:
+        return str(e)
 
 
-@app.route('/scrap', methods=['GET' , 'POST'])
-@cross_origin()
-def scrap():
+port = int(os.getenv("PORT", 5000))
+if __name__=="__main__":
+    app.run()
 
-    threadClass()
-    # Run(db_name=master_db_name)
-    return jsonify({'status': 'OK'})
-
-if __name__ == "__main__":
-    app.run()  # running the app on the local machine on port 8000
