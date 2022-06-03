68e3a517e5443ce7ffaabcff0ad158a442662e56
Prince Kurien <princevkurien@gmail.com>
2022-06-03 19:13:11 +0400

    timeout_issue

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from app_config.flask_config import FlaskAppConfiguration
-from app_exception.exception import AppException
-from app_entity.config_entity import FlaskConfig
+from app_pipeline.prediction_pipeline import PredictionPipeline
+from app_config.constants import *
+from app_util.util import read_yaml_file
 from flask import Flask, request
 from flask import send_file, abort, render_template ,request, jsonify
 from wsgiref import simple_server
@@ -11,7 +11,17 @@ import os
 import pandas as pd 
 
 
-
+config_info = read_yaml_file(file_path=CONFIG_FILE_PATH )
+model_pusher_config = config_info[MODEL_PUSHER_CONFIG_KEY]
+data_validation_config = config_info[DATA_VALIDATION_CONFIG_KEY]
+validation_config_dir = data_validation_config[DATA_VALIDATION_CONFIG_DIR]
+model_dir = os.path.join(ROOT_DIR, model_pusher_config[MODEL_PUSHER_MODEL_EXPORT_DIR_KEY])
+schema_file_path = os.path.join(
+    ROOT_DIR, validation_config_dir, data_validation_config[DATA_VALIDATION_SCHEMA_FILE_NAME_KEY])
+columns_transformer_dir = os.path.join(ROOT_DIR , model_pusher_config[PREPROCESSING_EXPORT_DIR_KEY])
+prediction_pipeline_obj = PredictionPipeline(model_dir=model_dir,\
+                                        columns_transformer_dir=columns_transformer_dir,\
+                                        schema_file_path=schema_file_path)
 
 app = Flask(__name__) # create the Flask app
 
@@ -25,7 +35,7 @@ def index():
     try:
         return render_template('index.html')
     except Exception as e:
-        return str(e)
+        return jsonify({'status': str(e)})
 
 @app.route('/eda', methods=['POST'])
 @cross_origin()
@@ -33,13 +43,12 @@ def eda():
     try:
         return render_template('eda.html')
     except Exception as e:
-        return str(e)
+        return jsonify({'status': str(e)})
 
 @app.route('/uploader', methods=['POST'])
 @cross_origin()
 def predictfile():
-    AppConfiguration = FlaskAppConfiguration()
-    prediction_config = AppConfiguration.get_flask_config()
+
     try:
         if request.method == 'POST':
             file = request.files['file']
@@ -48,7 +57,7 @@ def predictfile():
                 df = pd.read_csv(file)
                 print(df)
 
-                prediction = prediction_config.prediction_pipeline_obj.predict(df)
+                prediction = prediction_pipeline_obj.predict(df)
                 print(prediction)
                 return send_file(prediction, as_attachment=True)
             else:
@@ -59,14 +68,12 @@ def predictfile():
 @app.route('/predict', methods=['POST'])
 @cross_origin()
 def predict():
-    AppConfiguration = FlaskAppConfiguration()
-    prediction_config = AppConfiguration.get_flask_config()
     try:
         if request.method == 'POST':
             data = request.form.to_dict()
             df = pd.DataFrame(data , index = [0])
             print(df)
-            prediction = prediction_config.prediction_pipeline_obj.predict(df)
+            prediction = prediction_pipeline_obj.predict(df)
             
             return send_file(prediction, as_attachment=True)
             
@@ -74,7 +81,6 @@ def predict():
         return str(e)
 
 
-port = int(os.getenv("PORT", 5000))
 if __name__=="__main__":
     app.run()
 
