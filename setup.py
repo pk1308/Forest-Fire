8f89c815fcf032cb5d4bbed6a66dfda77030b644
Prince Kurien <princevkurien@gmail.com>
2022-06-03 20:08:08 +0400

    time out error

--- setup.py
+++ setup.py
@@ -1,20 +1,5 @@
 from setuptools import setup, find_packages
 
-REQUIREMENT_FILE_NAME = "requirements.txt"
-REMOVE_PACKAGE = "-e ."
-
-
-def get_requirement_list(requirement_file_name=REQUIREMENT_FILE_NAME) -> list:
-    try:
-        requirement_list = None
-        with open(requirement_file_name) as requirement_file:
-            requirement_list = [requirement.replace("\n", "") for requirement in requirement_file]
-            requirement_list.remove(REMOVE_PACKAGE)
-        return requirement_list
-    except Exception as e:
-        raise e
-
-
 setup(
     name="Forest Fire prediction",
     license="MIT",
@@ -22,5 +7,4 @@ setup(
     description="Project has been completed.",
     author="PK",
     packages=find_packages(),
-    install_requires=get_requirement_list()
 )
